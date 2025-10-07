898337ac1577b23daebd8e3dcccab962c5e13860
leosabbadin <leosabbadinads01@gmail.com>
2025-10-07 20:47:57 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-
 import type { Metadata } from 'next';
+import Script from 'next/script'; // 1. Importado o componente Script
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { WhatsAppButton } from '@/components/landing/whatsapp-button';
@@ -33,6 +33,30 @@ export default function RootLayout({
         {children}
         <WhatsAppButton />
         <Toaster />
+
+        {/* 2. CÓDIGO DO SEU PIXEL ADICIONADO AQUI */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1138030384495895');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1138030384495895&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
